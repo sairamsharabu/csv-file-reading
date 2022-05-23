edf5d5c741a5451c221ae8221d31135b8f3882c3
Mounika Pilli <Mounika.Pilli@bhavnacorp.com>
2022-05-23 19:53:39 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using LINQtoCSV;
+using System.Globalization;
+using EmployeeLibrary;
+
+
+
+namespace MLassigment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string filePath = @"D:\Csv formats\employeeinfo.csv";
+            
+            EmployeeProcessor Obj = new EmployeeProcessor();
+            var Result= Obj.ReadCsvFile(filePath);
+            Console.WriteLine(Result);
+            Console.ReadLine();
+
+        }
+       
+    }
+}
